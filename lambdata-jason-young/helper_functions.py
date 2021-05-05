4eca8782189683346ab34e137642ca3719c65e78
yaobviously <yaobviously@gmail.com>
2021-05-04 22:03:24 -0400

    added classes to my helper_functions.py

--- lambdata-jason-young/helper_functions.py
+++ lambdata-jason-young/helper_functions.py
@@ -1,21 +1,44 @@
 import pandas
 import numpy
 
-def null_count(df):
-    return df.isnull().sum().sum()
+"Three new classes for my helper functions"
+
+class NullCount:
+
+    def nulls(self, df):
+      
+      return df.isnull().sum().sum()
+
+
+class RandomizedDataFrame:
+  
+  def __init__(self, df, seed = 50):
+    self.df = df
+    self.randomseed = seed
+
+  def shuffle(self):
+    return self.df.sample(frac = 1, replace = False)
+
+
+class HungryDataFrame:
+
+  def __init__(self, listy, df):
+    self.listy = listy
+    self.df = df
+
+  
+  def mutate(self):
+    self.df['list'] = pd.Series(self.listy)
+
+    return self.df
+
+"One old function"
 
 def train_test_split(df, frac = 0.2):
     train = df.sample(frac = frac, replace = False, random_state = 60)
     test = df.drop(train.index)
     
     return train, test
-
-def randomize(df, seed = 50):
-    return df.sample(frac = 1, replace = False, random_seed = seed)
-
-def list_2_series(listtoseries, df):
-    df['list'] = pd.Series(listtoseries)
-    return df
 
 
 
